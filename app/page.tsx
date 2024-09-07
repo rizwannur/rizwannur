60e011922679df19ebb0ec70eeb24d10f252fd8e
Raf <rafeygx@gmail.com>
2024-09-07 15:31:57 +0600

    rollback

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { initialBlobityOptions } from "./utils/BlobityConfig";
 import NavBar from "./components/NavBar";
 
 import dynamic from "next/dynamic";
-//import Reviews from "./pages/reviews-section/ReviewGrid";
+import Reviews from "./pages/reviews-section/ReviewGrid";
 const Work = dynamic(() => import("./pages/work-section/Work"));
 const About = dynamic(() => import("./pages/about-section/About"));
 const Blog = dynamic(() => import("./pages/blog-section/BlogGrid"));
@@ -42,7 +42,7 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center">
         <Hero />
         <Work />
-        {/* <Reviews/> */}
+        <Reviews/>
         <About />
         <Blog />
         <Contact />
--- app/pages/reviews-section/reviewDetails.ts
+++ app/pages/reviews-section/reviewDetails.ts
@@ -16,53 +16,53 @@ export type reviewProps = {
 };
 
 export const reviewDetails = [
-  // {
-  //   name: "Samuel Alex",
-  //   role: "Product Designer",
-  //   company: "Umoja Linn",
-  //   profileImg: alex,
-  //   testimonial:
-  //     "Rafey is seriously one of the best frontend developers I've ever worked with. He's incredibly talented, super easy to work with, and always goes above and beyond.",
-  // },
-  // {
-  //   name: "Jeremiah Oloyede",
-  //   role: "Founder",
-  //   company: "Crown Branding Agency",
-  //   profileImg: jerry,
-  //   testimonial:
-  //     "Rafey's work is truly outstanding, and I can't recommend him enough! He completely transformed our branding agency's website. Since launch, we've seen a significant jump in website traffic and, crucially, conversions.",
-  // },
-  // {
-  //   name: "Mauro Reis",
-  //   role: "CEO",
-  //   company: "Meuclone Digital",
-  //   profileImg: mauro,
-  //   testimonial:
-  //     "Outstanding professional, developed our startup's website and delivered it in a very short time and with high quality.",
-  // },
-  // {
-  //   name: "Umar Abdullahi",
-  //   role: "Head of Engineering",
-  //   company: "Mono",
-  //   profileImg: umar,
-  //   testimonial:
-  //     "Rafey is hardworking and has great sense of ownership. He was able to contribute in engineering excellent user interfaces and user experiences at Mono. I’m confident he will be a great asset to any engineering team.",
-  // },
-  // {
-  //   name: "Olamide Sholanke",
-  //   role: "Senior Frontend Engineer",
-  //   company: "Mono",
-  //   profileImg: olamide,
-  //   testimonial:
-  //     "I had the pleasure of working with Rafey on a frontend development project at Mono, and I highly recommend him. He consistently delivered high-quality code, and brought a proactive and creative approach to problem-solving.",
-  // },
-  // {
-  //   name: "Alan Franciskovic",
-  //   role: "Sales Specialist",
-  //   company: "Croatia Osiguranje d.d",
-  //   profileImg: alan,
-  //   testimonial:
-  //     "Very talented and amazing freelancer. Very easy to communicate with, pleasure to work with. Outstanding attention to detail, and works as fast as possible while also delivering amazing work.",
-  // },
+  {
+    name: "Samuel Alex",
+    role: "Product Designer",
+    company: "Umoja Linn",
+    profileImg: alex,
+    testimonial:
+      "Rafey is seriously one of the best frontend developers I've ever worked with. He's incredibly talented, super easy to work with, and always goes above and beyond.",
+  },
+  {
+    name: "Jeremiah Oloyede",
+    role: "Founder",
+    company: "Crown Branding Agency",
+    profileImg: jerry,
+    testimonial:
+      "Rafey's work is truly outstanding, and I can't recommend him enough! He completely transformed our branding agency's website. Since launch, we've seen a significant jump in website traffic and, crucially, conversions.",
+  },
+  {
+    name: "Mauro Reis",
+    role: "CEO",
+    company: "Meuclone Digital",
+    profileImg: mauro,
+    testimonial:
+      "Outstanding professional, developed our startup's website and delivered it in a very short time and with high quality.",
+  },
+  {
+    name: "Umar Abdullahi",
+    role: "Head of Engineering",
+    company: "Mono",
+    profileImg: umar,
+    testimonial:
+      "Rafey is hardworking and has great sense of ownership. He was able to contribute in engineering excellent user interfaces and user experiences at Mono. I’m confident he will be a great asset to any engineering team.",
+  },
+  {
+    name: "Olamide Sholanke",
+    role: "Senior Frontend Engineer",
+    company: "Mono",
+    profileImg: olamide,
+    testimonial:
+      "I had the pleasure of working with Rafey on a frontend development project at Mono, and I highly recommend him. He consistently delivered high-quality code, and brought a proactive and creative approach to problem-solving.",
+  },
+  {
+    name: "Alan Franciskovic",
+    role: "Sales Specialist",
+    company: "Croatia Osiguranje d.d",
+    profileImg: alan,
+    testimonial:
+      "Very talented and amazing freelancer. Very easy to communicate with, pleasure to work with. Outstanding attention to detail, and works as fast as possible while also delivering amazing work.",
+  },
   
 ];
