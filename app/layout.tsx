6d7539b27748e85922a3d3a63fb78d204db6397e
Raf <rafeygx@gmail.com>
2024-09-07 22:51:20 +0600

    puush

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Syne } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const syne = Syne({
   subsets: ["latin"],
@@ -92,6 +93,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
